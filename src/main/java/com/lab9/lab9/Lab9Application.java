b86a845e1f77718cf1f3505167fefe59c2b39953
Andrew C <candetandrei@gmail.com>
2020-04-21 15:40:03 +0300

    Project build stage 1

---
+++ src/main/java/com/lab9/lab9/Lab9Application.java
@@ -0,0 +1,13 @@
+package com.lab9.lab9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab9/lab9/Lab9ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lab9.lab9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lab9ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
